3aed3dbf066e90b4656cdef60dff3b5140e50c76
BlokeAble <bcaballero54@gmail.com>
2022-08-10 11:24:56 -0600

    Added css and ITask model

--- ToDo/src/app/todo/todo.component.ts
+++ ToDo/src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  //Require and will be set later
+  todoForm !: FormGroup;
+  tasks : ITask [] =[];
+  inprogress : ITask[] = [];
+  done: ITask [] =[];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ['', Validators.required]
+    });
   }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
 
 }
